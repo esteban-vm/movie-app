1add14063a6391057e68cc8c10558688bf011d72
esteban-vm <estebanvm90@outlook.com>
2024-09-22 21:59:06 -0400

    create movie grid

---
+++ src/containers/movie.grid.tsx
@@ -0,0 +1,15 @@
+export default function MovieGrid() {
+  return (
+    <div className='flex flex-col items-center justify-center gap-1 rounded-lg bg-indigo-700 p-1 text-white'>
+      <h2>Movie Grid</h2>
+      <p className='text-center'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus libero quas minima labore cum magni blanditiis
+        repellat suscipit ut rem officia nobis quae placeat, error voluptas atque nemo sit quos!
+      </p>
+      <p className='text-center'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod non harum facere reprehenderit recusandae ducimus,
+        aspernatur quidem eaque error debitis.
+      </p>
+    </div>
+  )
+}
