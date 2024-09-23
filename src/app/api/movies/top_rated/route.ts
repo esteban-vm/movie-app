c20d3a46da6de656ed53ad8c55c1fa437abb2a8e
esteban-vm <estebanvm90@outlook.com>
2024-09-23 09:34:11 -0400

    create route for top rated movies

---
+++ src/app/api/movies/top_rated/route.ts
@@ -0,0 +1,58 @@
+import type { MovieData } from '@/types'
+import type { Movie } from 'tmdb-ts'
+import { getPlaceholderImage } from '@/utils'
+
+export async function GET() {
+  try {
+    const { topRated } = await import('@/mocks')
+
+    const movies = await Promise.all(
+      topRated.map(async (movie: Movie): Promise<MovieData> => {
+        const backdropPlaceholder = await getPlaceholderImage(movie.backdrop_path)
+        const posterPlaceholder = await getPlaceholderImage(movie.poster_path)
+
+        return {
+          ...movie,
+          backdrop_placeholder: backdropPlaceholder,
+          poster_placeholder: posterPlaceholder,
+        }
+      })
+    )
+
+    return Response.json(movies, { status: 200 })
+  } catch (error) {
+    return Response.json(error, { status: 500 })
+  }
+}
+
+// import type { MovieData } from '@/types'
+// import { tmdb } from '@/api/config'
+// import { API_IMAGE_URL } from '@/constants'
+// import { getPlaceholderImage } from '@/utils'
+
+// export async function GET() {
+//   try {
+//     const { results } = await tmdb.movies.topRated()
+
+//     const movies = await Promise.all(
+//       results.map(async ({ backdrop_path, poster_path, ...rest }): Promise<MovieData> => {
+//         const backdropPath = API_IMAGE_URL + backdrop_path
+//         const posterPath = API_IMAGE_URL + poster_path
+//         const backdropPlaceholder = await getPlaceholderImage(backdropPath)
+//         const posterPlaceholder = await getPlaceholderImage(posterPath)
+
+//         return {
+//           ...rest,
+//           backdrop_path: backdropPath,
+//           poster_path: posterPath,
+//           backdrop_placeholder: backdropPlaceholder,
+//           poster_placeholder: posterPlaceholder,
+//         }
+//       })
+//     )
+
+//     return Response.json(movies, { status: 200 })
+//   } catch (error) {
+//     return Response.json(error, { status: 500 })
+//   }
+// }
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,1 +1,2 @@
+export { default as topRated } from './top_rated_movies.json'
 export { default as upcoming } from './upcoming_movies.json'
