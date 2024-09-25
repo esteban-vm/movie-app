baaf1c009c445fb561f54d576c32c9418bfc9ead
esteban-vm <estebanvm90@outlook.com>
2024-09-25 11:45:42 -0400

    create load more button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 'use client'
 
+export { default as LoadMoreButton } from './load-more.button'
 export { default as MovieCard } from './movie.card'
 export { default as MovieItem } from './movie.item'
 export { default as MovieSlide } from './movie.slide'
